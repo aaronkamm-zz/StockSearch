636147fe243fc70cad11f38e72ac9e163d350507
aaron <aaronakamm@gmail.com>
2019-04-10 15:30:08 +0300

    Conditional rendering to handle invalid quote

--- client/src/components/StockComponent.js
+++ client/src/components/StockComponent.js
@@ -4,16 +4,20 @@ import Moment from 'react-moment';
 
 const StockComponent = ({latest: {symbol, price, high, low, latestDay}}) => (
   <div className = "card mt-4">
-    <div className = "card-header">
+    {(symbol) ? (<div className = "card-header">
       <h3>Info for {symbol} (as of <Moment format = "MMM Do, YYYY">{latestDay}</Moment>)</h3>
-    </div>
+    </div>): null}
       
-    <div className = "card-body">
+    {(price, high, low, latestDay) ? (<div className = "card-body">
       <h4>Current Price: {price}</h4>
       <h4>High: {high}</h4>
       <h4>Low: {low}</h4>
       <Link className = "btn btn-warning mt-3" to = '/'> Back to Search</Link>
-    </div>
+    </div>) : 
+    <div className = "card-body text-center">
+      <p class = "card-text">You've entered an incorrect quote. Go back and try again.</p>
+      <Link className = "btn btn-warning mt-3" to = '/'> Back to Search</Link>
+    </div>}
   </div> 
 );
 
